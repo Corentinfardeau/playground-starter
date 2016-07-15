49c3ad72d2b54ed9b68fe1088565d8483578cfb4
Corentin <corentin@Administrators-iMac.local>
2016-07-14 17:00:31 -0700

    init commit

---
+++ src/gulp/tasks/assets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp    = require('gulp'),
+browserSync = require('browser-sync'),
+config      = require('../config');
+
+gulp.task('assets', function(){
+  return gulp.src(config.copyBase + '**/*')
+	.pipe(gulp.dest(config.copyDest))
+	.pipe(browserSync.reload({stream:true}));
+});
